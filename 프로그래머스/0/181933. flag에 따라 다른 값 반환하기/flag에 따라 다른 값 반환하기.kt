4e8d63d2c5545f66eef1d342b94c51ace313fdf7
김상백 / Sangbaek Kim <95380638+tkdqor@users.noreply.github.com>
2024-06-08 01:20:54 +0900

    [level 0] Title: flag에 따라 다른 값 반환하기, Time: 0.01 ms, Memory: 62.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun solution(a: Int, b: Int, flag: Boolean): Int {
+        if (flag == true) {
+            return a + b
+        } else {
+            return a - b
+        }
+    }
+}
