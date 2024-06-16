75614840e82b89c7c54392591dc57a418e96b82b
김상백 / Sangbaek Kim <95380638+tkdqor@users.noreply.github.com>
2024-06-16 17:04:27 +0900

    Update 수 조작하기 1.kt

--- 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.kt
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.kt
@@ -3,6 +3,7 @@ class Solution {
         var result = n
         control.forEach { it ->
             // string 1개를 비교할 땐 "w"가 아니라 'w'
+            // 어떤 변수에 수를 더하고 그 더한 값을 사용하기 위해서는 단순힌 +,-가 아닌 +=, -=를 사용하기
             if (it == 'w') {
                 result += 1
             } else if (it == 's') {
